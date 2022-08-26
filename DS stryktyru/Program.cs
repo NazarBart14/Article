6d4ca5dc88a112fe39eb41a4466ace0d01d658e7
Lenovo <Lenovo@DESKTOP-AJ5INH1>
2022-08-26 20:32:44 +0300

    Добавьте файлы проекта.

---
+++ DS stryktyru/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DS_stryktyru
+{
+    struct Article
+    {
+        public double Kod;
+        public string Name;
+        public double Money;
+
+        public Article(double kod, double money , string name) : this()
+        {
+            Kod = kod;
+            Money = money;
+            Name = name;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine($"Money {Money}, Kod {Kod}, Name {Name}.");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double kod = 734235235, money = 1999;
+            string name = "Call of Duti: Warzon";
+            Article article = new Article(kod, money, name);
+            article.Print();
+        }
+    }
+
+}
